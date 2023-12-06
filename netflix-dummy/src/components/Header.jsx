9dd4518c05053613608ecf69bf2ab4959180ff0b
priyanka9725 <priyanka97xx@gmail.com>
2023-12-06 18:53:59 +0530

    signup changed

--- netflix-dummy/src/components/Header.jsx
+++ netflix-dummy/src/components/Header.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
-
-export default function Header() {
+import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+export default function Header(props) {
+  const navigate = useNavigate();
   return (
-    <div>Header</div>
-  )
+    <Container className="flex a-center j-between">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <button onClick={() => navigate(props.login ? "/login" : "Signup")}>
+        {props.login ? "Log In" : "Sign In"}
+      </button>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+padding: 0.4 rem;
+.logo{
+  img{
+    height: 0.5 rem;
+  }
+}
+button{
+  padding: 0.5rem;
+  background-color:#e50914;
+  border: none;
+  cursor:pointer
+  color:white
+  border-radius: 0.2 rem
+  font-weight: bolder;
+  font-size: 1.05 rem
+}`;
--- netflix-dummy/src/pages/Signup.jsx
+++ netflix-dummy/src/pages/Signup.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import BackgroundImage from '../components/BackgroundImage';
-import Header from '../components/Header';
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
 export default function Signup() {
   return (
     <Container>
-        <BackgroundImage/>
-        <Header />
+      <BackgroundImage />
+      <div className="content">
+        <Header login />
         <div className="body flex column a-center jcenter">
-            <div className="text flex column">
-                <h1>Unlimited movies, TV shows and more</h1>
-                <h4>Watch anywhere, Cancel anytime</h4>
-                <h6>Ready to Watch? Enter your Email to Create or Restart Membership</h6>
-            </div>
-            <div className="form">
-                <input type="email" placeholder='Email Address' name='email' />
-                <input type='password' placeholder='Password' name='password' />
-                <button> Get Started</button>
-            </div>
-            <button>Log In</button>
+          <div className="text flex column">
+            <h1>Unlimited movies, TV shows and more</h1>
+            <h4>Watch anywhere, Cancel anytime</h4>
+            <h6>
+              Ready to Watch? Enter your Email to Create or Restart Membership
+            </h6>
+          </div>
+          <div className="form">
+            <input type="email" placeholder="Email Address" name="email" />
+            <input type="password" placeholder="Password" name="password" />
+            <button> Get Started</button>
+          </div>
+          <button>Log In</button>
         </div>
+      </div>
     </Container>
-  )
+  );
 }
+
+const Container = styled.div`
+  posiiton: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+  }
+`;

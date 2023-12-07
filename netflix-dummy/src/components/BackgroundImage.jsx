6515c00fe3726113e76eacaeb895dedc6e6aa798
priyanka9725 <priyanka97xx@gmail.com>
2023-12-07 16:13:55 +0530

    header changes

--- netflix-dummy/src/components/BackgroundImage.jsx
+++ netflix-dummy/src/components/BackgroundImage.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import background from "../assets/login.jpg";
 import styled from "styled-components";
 export default function BackgroundImage() {
-  return (
-    <Container>
-      <img src={background} alt="background" />
-    </Container>
+  return(
+  <Container>
+    <img src={background} alt="background" />
+  </Container>
   );
 }
 
--- netflix-dummy/src/components/Header.jsx
+++ netflix-dummy/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../assets/logo.png";
-import { useNavigate } from "react-router-dom";
 export default function Header(props) {
   const navigate = useNavigate();
   return (
@@ -9,7 +9,7 @@ export default function Header(props) {
       <div className="logo">
         <img src={logo} alt="logo" />
       </div>
-      <button onClick={() => navigate(props.login ? "/login" : "Signup")}>
+      <button onClick={() => navigate(props.login ? "/login" : "signup")}>
         {props.login ? "Log In" : "Sign In"}
       </button>
     </Container>
@@ -17,19 +17,20 @@ export default function Header(props) {
 }
 
 const Container = styled.div`
-padding: 0.4 rem;
-.logo{
-  img{
-    height: 0.5 rem;
+  padding: 0 4rem;
+  .logo {
+    img {
+      height: 5rem;
+    }
   }
-}
-button{
-  padding: 0.5rem;
-  background-color:#e50914;
-  border: none;
-  cursor:pointer
-  color:white
-  border-radius: 0.2 rem
-  font-weight: bolder;
-  font-size: 1.05 rem
-}`;
+  button {
+    padding: 0.5rem 1rem;
+    background-color: #e50914;
+    border: none;
+    cursor: pointer;
+    color: white;
+    border-radius: 0.2rem;
+    font-weight: bolder;
+    font-size: 1.05rem;
+  }
+`;

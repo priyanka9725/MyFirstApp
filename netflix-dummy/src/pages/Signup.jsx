3e8cfa540330501360034b4e8088ee167723a886
priyanka9725 <priyanka97xx@gmail.com>
2023-12-09 22:08:58 +0530

    minor changes

--- netflix-dummy/src/pages/Signup.jsx
+++ netflix-dummy/src/pages/Signup.jsx
@@ -94,30 +94,30 @@ const Container = styled.div`
     width: 100vw;
     display: grid;
     grid-template-rows: 15vh 85vh;
-    .body{
-      gap:1rem;
+    .body {
+      gap: 1rem;
       text-align: center;
       font-size: 2rem;
-      h1{
+      h1 {
         padding: 0 20rem;
       }
     }
-    .form{
+    .form {
       display: grid;
       grid-template-columns: ${({ showPassword }) =>
         showPassword ? "1fr 1fr" : "2fr 1fr"};
       width: 60%;
-      input{
-        color:black;
+      input {
+        color: black;
         border: none;
         padding: 1.5rem;
         font-size: 1.2rem;
         border: 1px solid black;
-        &:focus{
-          outline:none;
+        &:focus {
+          outline: none;
         }
       }
-      button{
+      button {
         padding: 0.5rem 1rem;
         background-color: #e50914;
         border: none;
@@ -128,15 +128,15 @@ const Container = styled.div`
         font-size: 1.05rem;
       }
     }
-    button{
-        padding: 0.5rem 1rem;
-        background-color: #e50914;
-        border: none;
-        cursor: pointer;
-        color: white;
-        border-radius: 0.2rem;
-        font-weight: bolder;
-        font-size: 1.05rem;
+    button {
+      padding: 0.5rem 1rem;
+      background-color: #e50914;
+      border: none;
+      cursor: pointer;
+      color: white;
+      border-radius: 0.2rem;
+      font-weight: bolder;
+      font-size: 1.05rem;
+    }
   }
-}
 `;
